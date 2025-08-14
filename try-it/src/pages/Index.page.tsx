717aa878435886faeccae543685fc4d1f05cccc0
juanlopez1 <juan_manuel_lopez_1996@hotmail.com>
2025-08-14 14:08:38 -0300

    feat: add shadcn and animate-ui

--- try-it/src/pages/Index.page.tsx
+++ try-it/src/pages/Index.page.tsx
@@ -1,12 +1,22 @@
-import { modules } from '@/constants/modules';
-import { ModuleCard } from '@/components/molecules/ModuleCard.molecule';
+import { useNavigate } from 'react-router-dom';
 
-const IndexPage = () => (
-    <div className="index__container">
-        {modules.map((module) => (
-            <ModuleCard key={module.path} module={module} />
-        ))}
-    </div>
-);
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/atoms/Card.atom';
+import { modules } from '@/constants/modules';
+
+const IndexPage = () => {
+    const navigate = useNavigate();
+    return (
+        <div className="index">
+            {modules.map((module) => (
+                <Card key={module.path} className="w-[300px]" onClick={() => navigate(module.path)}>
+                    <CardHeader>
+                        <CardTitle>{module.title}</CardTitle>
+                        <CardDescription>{module.description}</CardDescription>
+                    </CardHeader>
+                </Card>
+            ))}
+        </div>
+    );
+};
 
 export default IndexPage;
--- try-it/src/pages/SRP.page.tsx
+++ try-it/src/pages/SRP.page.tsx
@@ -1,5 +1,5 @@
 const SRPPage = () => (
-    <div className="srp__container">
+    <div className="single-responsibility-principle">
         <h2 className="text-3xl mb-2">1 - Principio de responsabilidad única</h2>
         <ul className="list-disc">
             <li>Una entidad (función, clase, componente, hook) debería tener un único motivo para cambiar.</li>
